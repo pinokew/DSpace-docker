DSpace/DSpace

[CST-10704][CST-14902] Adds ORCID login flow with private email

dspace-api/src/main/java/org/dspace/eperson/RegistrationDataMetadata.java
@@ -1,0 +1,109 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.eperson;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.dspace.content.MetadataField;
+import org.dspace.core.ReloadableEntity;
+import org.hibernate.annotations.Type;
+
+/**
+ * Metadata related to a registration data {@link RegistrationData}
+ *
+ * @author Vincenzo Mecca (vins01-4science - vincenzo.mecca at 4science.com)
+ **/
+@Entity
+@Table(name = "registrationdata_metadata")
+public class RegistrationDataMetadata implements ReloadableEntity<Integer>, Comparable<RegistrationDataMetadata> {
+
+    @Id
+    @Column(name = "registrationdata_metadata_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "registrationdata_metadatavalue_seq")
+    @SequenceGenerator(
+        name = "registrationdata_metadatavalue_seq",
+        sequenceName = "registrationdata_metadatavalue_seq",
+        allocationSize = 1
+    )
+    private final Integer id;
+
+    /**
+     * {@link RegistrationData} linked to this metadata value
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "registrationdata_id")
+    private RegistrationData registrationData = null;
+
+    /**
+     * The linked {@link MetadataField} instance
+     */
+    @ManyToOne
+    @JoinColumn(name = "metadata_field_id")
+    private MetadataField metadataField = null;
+
+    /**
+     * Value represented by this {@link RegistrationDataMetadata} instance
+     * related to the metadataField {@link MetadataField}
+     */
+    @Lob
+    @Type(type = "org.dspace.storage.rdbms.hibernate.DatabaseAwareLobType")
+    @Column(name = "text_value")
+    private String value = null;
+
+    /**
+     * Protected constructor
+     */
+    protected RegistrationDataMetadata() {
+        id = 0;
+    }
+
+
+    @Override
+    public Integer getID() {
+        return id;
+    }
+
+    public MetadataField getMetadataField() {
+        return metadataField;
+    }
+
+    void setMetadataField(MetadataField metadataField) {
+        this.metadataField = metadataField;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(RegistrationDataMetadata o) {
+        return Integer.compare(this.id, o.id);
+    }
+
+    void setRegistrationData(RegistrationData registrationData) {
+        this.registrationData = registrationData;
+    }
+
+    public RegistrationData getRegistrationData() {
+        return registrationData;
+    }
+}

dspace-api/src/main/java/org/dspace/eperson/dao/impl/RegistrationDataDAOImpl.java
@@ -8,8 +8,10 @@
 package org.dspace.eperson.dao.impl;
 
 import java.sql.SQLException;
+import java.util.Date;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
@@ -17,6 +19,7 @@ import org.dspace.core.AbstractHibernateDAO;
 import org.dspace.core.Context;
 import org.dspace.eperson.RegistrationData;
 import org.dspace.eperson.RegistrationData_;
+import org.dspace.eperson.RegistrationTypeEnum;
 import org.dspace.eperson.dao.RegistrationDataDAO;
 
 /**
@@ -43,6 +46,21 @@ public class RegistrationDataDAOImpl extends AbstractHibernateDAO<RegistrationDa
     }
 
     @Override
+    public RegistrationData findBy(Context context, String email, RegistrationTypeEnum type) throws SQLException {
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
+        CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RegistrationData.class);
+        Root<RegistrationData> registrationDataRoot = criteriaQuery.from(RegistrationData.class);
+        criteriaQuery.select(registrationDataRoot);
+        criteriaQuery.where(
+            criteriaBuilder.and(
+                criteriaBuilder.equal(registrationDataRoot.get(RegistrationData_.email), email),
+                criteriaBuilder.equal(registrationDataRoot.get(RegistrationData_.registrationType), type)
+            )
+        );
+        return uniqueResult(context, criteriaQuery, false, RegistrationData.class);
+    }
+
+    @Override
     public RegistrationData findByToken(Context context, String token) throws SQLException {
         CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
         CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RegistrationData.class);
@@ -58,5 +76,16 @@ public class RegistrationDataDAOImpl extends AbstractHibernateDAO<RegistrationDa
         Query query = createQuery(context, hql);
         query.setParameter("token", token);
         query.executeUpdate();
+    }
+
+    @Override
+    public void deleteExpiredBy(Context context, Date date) throws SQLException {
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
+        CriteriaDelete<RegistrationData> deleteQuery = criteriaBuilder.createCriteriaDelete(RegistrationData.class);
+        Root<RegistrationData> deleteRoot = deleteQuery.from(RegistrationData.class);
+        deleteQuery.where(
+            criteriaBuilder.lessThanOrEqualTo(deleteRoot.get(RegistrationData_.expires), date)
+        );
+        getHibernateSession(context).createQuery(deleteQuery).executeUpdate();
     }
 }

dspace-server-webapp/src/main/java/org/dspace/app/rest/EPersonRegistrationRestController.java
@@ -1,0 +1,86 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest;
+
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
+
+import org.dspace.app.rest.converter.ConverterService;
+import org.dspace.app.rest.model.EPersonRest;
+import org.dspace.app.rest.model.hateoas.EPersonResource;
+import org.dspace.app.rest.repository.EPersonRestRepository;
+import org.dspace.app.rest.utils.ContextUtil;
+import org.dspace.core.Context;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ControllerUtils;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Vincenzo Mecca (vins01-4science - vincenzo.mecca at 4science.com)
+ **/
+@RestController
+@RequestMapping("/api/" + EPersonRest.CATEGORY + "/" + EPersonRest.PLURAL_NAME)
+public class EPersonRegistrationRestController {
+
+    @Autowired
+    private EPersonRestRepository ePersonRestRepository;
+
+    @Autowired
+    private ConverterService converter;
+
+    /**
+     * This method will merge the data coming from a {@link org.dspace.eperson.RegistrationData} into the current
+     * logged-in user.
+     * <br/>
+     * The request must have an empty body, and a token parameter should be provided:
+     * <pre>
+     *  <code>
+     *   curl -X POST http://${dspace.url}/api/eperson/epersons/${id-eperson}?token=${token}&override=${metadata-fields}
+     *        -H "Content-Type: application/json"
+     *        -H "Authorization: Bearer ${bearer-token}"
+     *  </code>
+     * </pre>
+     * @param request httpServletRequest incoming
+     * @param uuid uuid of the eperson
+     * @param token registration token
+     * @param override fields to override inside from the registration data to the eperson
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/{uuid}")
+    public ResponseEntity<RepresentationModel<?>> post(
+        HttpServletRequest request,
+        @PathVariable String uuid,
+        @RequestParam @NotNull String token,
+        @RequestParam(required = false) List<String> override
+    ) throws Exception {
+        Context context = ContextUtil.obtainContext(request);
+        try {
+            context.turnOffAuthorisationSystem();
+            EPersonRest epersonRest =
+                ePersonRestRepository.mergeFromRegistrationData(context, UUID.fromString(uuid), token, override);
+            EPersonResource resource = converter.toResource(epersonRest);
+            return ControllerUtils.toResponseEntity(HttpStatus.CREATED, new HttpHeaders(), resource);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            context.restoreAuthSystemState();
+        }
+    }
+
+}

pinokew/DSpace-docker

dspace-api/src/main/java/org/dspace/eperson/RegistrationDataMetadata.java
@@ -1,0 +1,105 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.eperson;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import org.dspace.content.MetadataField;
+import org.dspace.core.ReloadableEntity;
+import org.hibernate.Length;
+
+/**
+ * Metadata related to a registration data {@link RegistrationData}
+ *
+ * @author Vincenzo Mecca (vins01-4science - vincenzo.mecca at 4science.com)
+ **/
+@Entity
+@Table(name = "registrationdata_metadata")
+public class RegistrationDataMetadata implements ReloadableEntity<Integer>, Comparable<RegistrationDataMetadata> {
+
+    @Id
+    @Column(name = "registrationdata_metadata_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "registrationdata_metadatavalue_seq")
+    @SequenceGenerator(
+        name = "registrationdata_metadatavalue_seq",
+        sequenceName = "registrationdata_metadatavalue_seq",
+        allocationSize = 1
+    )
+    private final Integer id;
+
+    /**
+     * {@link RegistrationData} linked to this metadata value
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "registrationdata_id")
+    private RegistrationData registrationData = null;
+
+    /**
+     * The linked {@link MetadataField} instance
+     */
+    @ManyToOne
+    @JoinColumn(name = "metadata_field_id")
+    private MetadataField metadataField = null;
+
+    /**
+     * Value represented by this {@link RegistrationDataMetadata} instance
+     * related to the metadataField {@link MetadataField}
+     */
+    @Column(name = "text_value", length = Length.LONG32)
+    private String value = null;
+
+    /**
+     * Protected constructor
+     */
+    protected RegistrationDataMetadata() {
+        id = 0;
+    }
+
+
+    @Override
+    public Integer getID() {
+        return id;
+    }
+
+    public MetadataField getMetadataField() {
+        return metadataField;
+    }
+
+    void setMetadataField(MetadataField metadataField) {
+        this.metadataField = metadataField;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(RegistrationDataMetadata o) {
+        return Integer.compare(this.id, o.id);
+    }
+
+    void setRegistrationData(RegistrationData registrationData) {
+        this.registrationData = registrationData;
+    }
+
+    public RegistrationData getRegistrationData() {
+        return registrationData;
+    }
+}

dspace-api/src/main/java/org/dspace/eperson/dao/impl/RegistrationDataDAOImpl.java
@@ -8,15 +8,18 @@
 package org.dspace.eperson.dao.impl;
 
 import java.sql.SQLException;
+import java.util.Date;
 
 import jakarta.persistence.Query;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaDelete;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import org.dspace.core.AbstractHibernateDAO;
 import org.dspace.core.Context;
 import org.dspace.eperson.RegistrationData;
 import org.dspace.eperson.RegistrationData_;
+import org.dspace.eperson.RegistrationTypeEnum;
 import org.dspace.eperson.dao.RegistrationDataDAO;
 
 /**
@@ -43,6 +46,21 @@ public class RegistrationDataDAOImpl extends AbstractHibernateDAO<RegistrationDa
     }
 
     @Override
+    public RegistrationData findBy(Context context, String email, RegistrationTypeEnum type) throws SQLException {
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
+        CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RegistrationData.class);
+        Root<RegistrationData> registrationDataRoot = criteriaQuery.from(RegistrationData.class);
+        criteriaQuery.select(registrationDataRoot);
+        criteriaQuery.where(
+            criteriaBuilder.and(
+                criteriaBuilder.equal(registrationDataRoot.get(RegistrationData_.email), email),
+                criteriaBuilder.equal(registrationDataRoot.get(RegistrationData_.registrationType), type)
+            )
+        );
+        return uniqueResult(context, criteriaQuery, false, RegistrationData.class);
+    }
+
+    @Override
     public RegistrationData findByToken(Context context, String token) throws SQLException {
         CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
         CriteriaQuery criteriaQuery = getCriteriaQuery(criteriaBuilder, RegistrationData.class);
@@ -58,5 +76,16 @@ public class RegistrationDataDAOImpl extends AbstractHibernateDAO<RegistrationDa
         Query query = createQuery(context, hql);
         query.setParameter("token", token);
         query.executeUpdate();
+    }
+
+    @Override
+    public void deleteExpiredBy(Context context, Date date) throws SQLException {
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(context);
+        CriteriaDelete<RegistrationData> deleteQuery = criteriaBuilder.createCriteriaDelete(RegistrationData.class);
+        Root<RegistrationData> deleteRoot = deleteQuery.from(RegistrationData.class);
+        deleteQuery.where(
+            criteriaBuilder.lessThanOrEqualTo(deleteRoot.get(RegistrationData_.expires), date)
+        );
+        getHibernateSession(context).createQuery(deleteQuery).executeUpdate();
     }
 }

dspace-server-webapp/src/main/java/org/dspace/app/rest/EPersonRegistrationRestController.java
@@ -1,0 +1,86 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotNull;
+import org.dspace.app.rest.converter.ConverterService;
+import org.dspace.app.rest.model.EPersonRest;
+import org.dspace.app.rest.model.hateoas.EPersonResource;
+import org.dspace.app.rest.repository.EPersonRestRepository;
+import org.dspace.app.rest.utils.ContextUtil;
+import org.dspace.core.Context;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ControllerUtils;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Vincenzo Mecca (vins01-4science - vincenzo.mecca at 4science.com)
+ **/
+@RestController
+@RequestMapping("/api/" + EPersonRest.CATEGORY + "/" + EPersonRest.PLURAL_NAME)
+public class EPersonRegistrationRestController {
+
+    @Autowired
+    private EPersonRestRepository ePersonRestRepository;
+
+    @Autowired
+    private ConverterService converter;
+
+    /**
+     * This method will merge the data coming from a {@link org.dspace.eperson.RegistrationData} into the current
+     * logged-in user.
+     * <br/>
+     * The request must have an empty body, and a token parameter should be provided:
+     * <pre>
+     *  <code>
+     *   curl -X POST http://${dspace.url}/api/eperson/epersons/${id-eperson}?token=${token}&override=${metadata-fields}
+     *        -H "Content-Type: application/json"
+     *        -H "Authorization: Bearer ${bearer-token}"
+     *  </code>
+     * </pre>
+     * @param request httpServletRequest incoming
+     * @param uuid uuid of the eperson
+     * @param token registration token
+     * @param override fields to override inside from the registration data to the eperson
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/{uuid}")
+    public ResponseEntity<RepresentationModel<?>> post(
+        HttpServletRequest request,
+        @PathVariable String uuid,
+        @RequestParam @NotNull String token,
+        @RequestParam(required = false) List<String> override
+    ) throws Exception {
+        Context context = ContextUtil.obtainContext(request);
+        try {
+            context.turnOffAuthorisationSystem();
+            EPersonRest epersonRest =
+                ePersonRestRepository.mergeFromRegistrationData(context, UUID.fromString(uuid), token, override);
+            EPersonResource resource = converter.toResource(epersonRest);
+            return ControllerUtils.toResponseEntity(HttpStatus.CREATED, new HttpHeaders(), resource);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            context.restoreAuthSystemState();
+        }
+    }
+
+}