ray-lee/DSpace

Add SAML login filter.

dspace-server-webapp/src/main/java/org/dspace/app/rest/security/SamlLoginFilter.java
@@ -1,0 +1,119 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.security;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dspace.authenticate.SamlAuthentication;
+import org.dspace.core.Utils;
+import org.dspace.services.ConfigurationService;
+import org.dspace.services.factory.DSpaceServicesFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+/**
+ * A filter that examines requests to see if the user has been authenticated via SAML.
+ * <p>
+ * The overall SAML login process is as follows:
+ * </p>
+ * <ol>
+ *   <li>When SAML authentication is enabled, the client/UI receives the URL to the active SAML
+ *       relying party's authentication endpoint in the WWW-Authenticate header.
+ *       See {@link org.dspace.authenticate.SamlAuthentication#loginPageURL(org.dspace.core.Context, HttpServletRequest, HttpServletResponse)}.</li>
+ *   <li>The client sends the user to that URL when they select SAML authentication.</li>
+ *   <li>The active SAML relying party sends the client to the login page at the asserting party
+ *       (aka identity provider, or IdP).</li>
+ *   <li>The user logs in to the asserting party.</li>
+ *   <li>If successful, the asserting party sends the client back to the relying party's assertion
+ *       consumer endpoint, along with the SAML assertion.</li>
+ *   <li>The relying party receives the SAML assertion, extracts attributes from the assertion,
+ *       maps them into request attributes, and forwards the request to the path where this filter
+ *       is listening.</li>
+ *   <li>This filter intercepts the request in order to check for a valid SAML login (see
+ *       {@link org.dspace.authenticate.SamlAuthentication#authenticate(org.dspace.core.Context, String, String, String, HttpServletRequest)})
+ *       and stores that user info in a JWT. It also saves that JWT in a <em>temporary</em>
+ *       authentication cookie.</li>
+ *   <li>This filter redirects the user back to the UI (after verifying it's at a trusted URL).</li>
+ *   <li>The client reads the JWT from the cookie, and sends it back in a request to
+ *       /api/authn/login, which triggers the server-side to destroy the cookie and move the JWT
+ *       into a header.</li>
+ * </ol>
+ *
+ * @author Ray Lee
+ */
+public class SamlLoginFilter extends StatelessLoginFilter {
+    private static final Logger logger = LogManager.getLogger(SamlLoginFilter.class);
+
+    private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+
+    public SamlLoginFilter(String url, AuthenticationManager authenticationManager,
+            RestAuthenticationService restAuthenticationService) {
+        super(url, authenticationManager, restAuthenticationService);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+        throws AuthenticationException {
+
+        if (!SamlAuthentication.isEnabled()) {
+            throw new ProviderNotFoundException("SAML is disabled.");
+        }
+
+        // Because this authentication is implicit, we pass in an empty DSpaceAuthentication.
+        return authenticationManager.authenticate(new DSpaceAuthentication());
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+        Authentication auth) throws IOException, ServletException {
+
+        restAuthenticationService.addAuthenticationDataForUser(request, response, (DSpaceAuthentication) auth, true);
+
+        redirectAfterSuccess(request, response);
+    }
+
+    /**
+     * After successful login, redirect to the configured UI URL. If that URL is not allowed for
+     * this DSpace site, return a 400 error.
+     *
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    private void redirectAfterSuccess(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String redirectUrl = configurationService.getProperty("dspace.ui.url");
+        String redirectHostName = Utils.getHostName(redirectUrl);
+        String serverUrl = configurationService.getProperty("dspace.server.url");
+
+        boolean isRedirectAllowed = Stream.concat(
+                Stream.of(serverUrl),
+                Arrays.stream(configurationService.getArrayProperty("rest.cors.allowed-origins")))
+            .map(url -> Utils.getHostName(url))
+            .anyMatch(hostName -> hostName.equalsIgnoreCase(redirectHostName));
+
+        if (isRedirectAllowed) {
+            logger.debug("SAML redirecting to " + redirectUrl);
+
+            response.sendRedirect(redirectUrl);
+        } else {
+            logger.error("SAML redirect URL {} is not allowed" + redirectUrl);
+
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"SAML redirect URL not allowed");
+        }
+    }
+}

dspace-server-webapp/src/test/java/org/dspace/app/rest/security/SamlLoginFilterTest.java
@@ -1,0 +1,164 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.security;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dspace.AbstractDSpaceTest;
+import org.dspace.servicemanager.config.DSpaceConfigurationService;
+import org.dspace.services.ConfigurationService;
+import org.dspace.services.factory.DSpaceServicesFactory;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderNotFoundException;
+import org.springframework.security.core.Authentication;
+
+public class SamlLoginFilterTest extends AbstractDSpaceTest {
+    private static ConfigurationService configurationService;
+
+    private AuthenticationManager authManager;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private RestAuthenticationService restAuthService;
+    private FilterChain filterChain;
+    private SamlLoginFilter filter;
+
+    @BeforeClass
+    public static void beforeAll() {
+        configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+    }
+
+    @Before
+    public void beforeEach() throws Exception {
+        resetConfigurationService();
+
+        authManager = createAuthenticationManager();
+        restAuthService = createRestAuthenticationService();
+        filterChain = Mockito.mock(FilterChain.class);
+        filter = new SamlLoginFilter("/api/authn/saml", authManager, restAuthService);
+        request = createRequest("/api/authn/saml");
+        response = createResponse();
+    }
+
+    @Test
+    public void testRedirectAfterSuccess() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("dspace.ui.url","http://dspace.example.org");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(response).sendRedirect("http://dspace.example.org");
+    }
+
+    @Test
+    public void testRedirectToRemoteHostNotAllowed() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("dspace.ui.url","http://different.host.bad");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(response).sendError(eq(400), anyString());
+    }
+
+    @Test
+    public void testRedirectToRemoteHostCorsAllowed() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("rest.cors.allowed-origins", "http://different.host.ok");
+        configurationService.setProperty("dspace.ui.url","http://different.host.ok");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(response).sendRedirect("http://different.host.ok");
+    }
+
+    @Test
+    public void testAuthCookieSaved() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("dspace.ui.url","http://dspace.example.org");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(restAuthService).addAuthenticationDataForUser(
+            eq(request), eq(response), any(DSpaceAuthentication.class), eq(true));
+    }
+
+    @Test
+    public void testSamlAuthenticationNotEnabled() throws Exception {
+        assertThrows(ProviderNotFoundException.class, () -> filter.attemptAuthentication(request, response));
+    }
+
+    private void resetConfigurationService() {
+        ((DSpaceConfigurationService) configurationService).clear();
+
+        configurationService.reloadConfig();
+    }
+
+    private AuthenticationManager createAuthenticationManager() {
+        AuthenticationManager mockAuthManager = Mockito.mock(AuthenticationManager.class);
+
+        when(mockAuthManager.authenticate(any(Authentication.class)))
+            .thenReturn(Mockito.mock(DSpaceAuthentication.class));
+
+        return mockAuthManager;
+    }
+
+    private RestAuthenticationService createRestAuthenticationService() throws Exception {
+        RestAuthenticationService mockRestAuthService = Mockito.mock(RestAuthenticationService.class);
+
+        doNothing().when(mockRestAuthService).addAuthenticationDataForUser(
+            isA(HttpServletRequest.class), isA(HttpServletResponse.class),
+            isA(DSpaceAuthentication.class), isA(Boolean.class));
+
+        return mockRestAuthService;
+    }
+
+    private HttpServletRequest createRequest(String path) {
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest(HttpMethod.GET.name(), path);
+
+        mockRequest.setPathInfo(path);
+
+        return mockRequest;
+    }
+
+    private HttpServletResponse createResponse() throws Exception {
+        HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+
+        doNothing().when(mockResponse).sendRedirect(isA(String.class));
+        doNothing().when(mockResponse).sendError(isA(Integer.class), isA(String.class));
+
+        return mockResponse;
+    }
+}

pinokew/DSpace-docker

dspace-server-webapp/src/main/java/org/dspace/app/rest/security/SamlLoginFilter.java
@@ -1,0 +1,119 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.security;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dspace.authenticate.SamlAuthentication;
+import org.dspace.core.Utils;
+import org.dspace.services.ConfigurationService;
+import org.dspace.services.factory.DSpaceServicesFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+/**
+ * A filter that examines requests to see if the user has been authenticated via SAML.
+ * <p>
+ * The overall SAML login process is as follows:
+ * </p>
+ * <ol>
+ *   <li>When SAML authentication is enabled, the client/UI receives the URL to the active SAML
+ *       relying party's authentication endpoint in the WWW-Authenticate header.
+ *       See {@link org.dspace.authenticate.SamlAuthentication#loginPageURL(org.dspace.core.Context, HttpServletRequest, HttpServletResponse)}.</li>
+ *   <li>The client sends the user to that URL when they select SAML authentication.</li>
+ *   <li>The active SAML relying party sends the client to the login page at the asserting party
+ *       (aka identity provider, or IdP).</li>
+ *   <li>The user logs in to the asserting party.</li>
+ *   <li>If successful, the asserting party sends the client back to the relying party's assertion
+ *       consumer endpoint, along with the SAML assertion.</li>
+ *   <li>The relying party receives the SAML assertion, extracts attributes from the assertion,
+ *       maps them into request attributes, and forwards the request to the path where this filter
+ *       is listening.</li>
+ *   <li>This filter intercepts the request in order to check for a valid SAML login (see
+ *       {@link org.dspace.authenticate.SamlAuthentication#authenticate(org.dspace.core.Context, String, String, String, HttpServletRequest)})
+ *       and stores that user info in a JWT. It also saves that JWT in a <em>temporary</em>
+ *       authentication cookie.</li>
+ *   <li>This filter redirects the user back to the UI (after verifying it's at a trusted URL).</li>
+ *   <li>The client reads the JWT from the cookie, and sends it back in a request to
+ *       /api/authn/login, which triggers the server-side to destroy the cookie and move the JWT
+ *       into a header.</li>
+ * </ol>
+ *
+ * @author Ray Lee
+ */
+public class SamlLoginFilter extends StatelessLoginFilter {
+    private static final Logger logger = LogManager.getLogger(SamlLoginFilter.class);
+
+    private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+
+    public SamlLoginFilter(String url, String httpMethod, AuthenticationManager authenticationManager,
+            RestAuthenticationService restAuthenticationService) {
+        super(url, httpMethod, authenticationManager, restAuthenticationService);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+        throws AuthenticationException {
+
+        if (!SamlAuthentication.isEnabled()) {
+            throw new ProviderNotFoundException("SAML is disabled.");
+        }
+
+        // Because this authentication is implicit, we pass in an empty DSpaceAuthentication.
+        return authenticationManager.authenticate(new DSpaceAuthentication());
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+        Authentication auth) throws IOException, ServletException {
+
+        restAuthenticationService.addAuthenticationDataForUser(request, response, (DSpaceAuthentication) auth, true);
+
+        redirectAfterSuccess(request, response);
+    }
+
+    /**
+     * After successful login, redirect to the configured UI URL. If that URL is not allowed for
+     * this DSpace site, return a 400 error.
+     *
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    private void redirectAfterSuccess(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String redirectUrl = configurationService.getProperty("dspace.ui.url");
+        String redirectHostName = Utils.getHostName(redirectUrl);
+        String serverUrl = configurationService.getProperty("dspace.server.url");
+
+        boolean isRedirectAllowed = Stream.concat(
+                Stream.of(serverUrl),
+                Arrays.stream(configurationService.getArrayProperty("rest.cors.allowed-origins")))
+            .map(url -> Utils.getHostName(url))
+            .anyMatch(hostName -> hostName.equalsIgnoreCase(redirectHostName));
+
+        if (isRedirectAllowed) {
+            logger.debug("SAML redirecting to " + redirectUrl);
+
+            response.sendRedirect(redirectUrl);
+        } else {
+            logger.error("SAML redirect URL {} is not allowed" + redirectUrl);
+
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"SAML redirect URL not allowed");
+        }
+    }
+}

dspace-server-webapp/src/test/java/org/dspace/app/rest/security/SamlLoginFilterTest.java
@@ -1,0 +1,163 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.security;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.dspace.AbstractDSpaceTest;
+import org.dspace.servicemanager.config.DSpaceConfigurationService;
+import org.dspace.services.ConfigurationService;
+import org.dspace.services.factory.DSpaceServicesFactory;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderNotFoundException;
+import org.springframework.security.core.Authentication;
+
+public class SamlLoginFilterTest extends AbstractDSpaceTest {
+    private static ConfigurationService configurationService;
+
+    private AuthenticationManager authManager;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private RestAuthenticationService restAuthService;
+    private FilterChain filterChain;
+    private SamlLoginFilter filter;
+
+    @BeforeClass
+    public static void beforeAll() {
+        configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
+    }
+
+    @Before
+    public void beforeEach() throws Exception {
+        resetConfigurationService();
+
+        authManager = createAuthenticationManager();
+        restAuthService = createRestAuthenticationService();
+        filterChain = Mockito.mock(FilterChain.class);
+        filter = new SamlLoginFilter("/api/authn/saml", HttpMethod.GET.name(), authManager, restAuthService);
+        request = createRequest("/api/authn/saml");
+        response = createResponse();
+    }
+
+    @Test
+    public void testRedirectAfterSuccess() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("dspace.ui.url","http://dspace.example.org");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(response).sendRedirect("http://dspace.example.org");
+    }
+
+    @Test
+    public void testRedirectToRemoteHostNotAllowed() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("dspace.ui.url","http://different.host.bad");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(response).sendError(eq(400), anyString());
+    }
+
+    @Test
+    public void testRedirectToRemoteHostCorsAllowed() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("rest.cors.allowed-origins", "http://different.host.ok");
+        configurationService.setProperty("dspace.ui.url","http://different.host.ok");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(response).sendRedirect("http://different.host.ok");
+    }
+
+    @Test
+    public void testAuthCookieSaved() throws Exception {
+        configurationService.setProperty("plugin.sequence.org.dspace.authenticate.AuthenticationMethod",
+            "org.dspace.authenticate.SamlAuthentication");
+
+        configurationService.setProperty("dspace.ui.url","http://dspace.example.org");
+        configurationService.setProperty("dspace.server.url","http://dspace.example.org/server");
+
+        filter.doFilter(request, response, filterChain);
+
+        verify(restAuthService).addAuthenticationDataForUser(
+            eq(request), eq(response), any(DSpaceAuthentication.class), eq(true));
+    }
+
+    @Test
+    public void testSamlAuthenticationNotEnabled() throws Exception {
+        assertThrows(ProviderNotFoundException.class, () -> filter.attemptAuthentication(request, response));
+    }
+
+    private void resetConfigurationService() {
+        ((DSpaceConfigurationService) configurationService).clear();
+
+        configurationService.reloadConfig();
+    }
+
+    private AuthenticationManager createAuthenticationManager() {
+        AuthenticationManager mockAuthManager = Mockito.mock(AuthenticationManager.class);
+
+        when(mockAuthManager.authenticate(any(Authentication.class)))
+            .thenReturn(Mockito.mock(DSpaceAuthentication.class));
+
+        return mockAuthManager;
+    }
+
+    private RestAuthenticationService createRestAuthenticationService() throws Exception {
+        RestAuthenticationService mockRestAuthService = Mockito.mock(RestAuthenticationService.class);
+
+        doNothing().when(mockRestAuthService).addAuthenticationDataForUser(
+            isA(HttpServletRequest.class), isA(HttpServletResponse.class),
+            isA(DSpaceAuthentication.class), isA(Boolean.class));
+
+        return mockRestAuthService;
+    }
+
+    private HttpServletRequest createRequest(String path) {
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest(HttpMethod.GET.name(), path);
+
+        mockRequest.setPathInfo(path);
+
+        return mockRequest;
+    }
+
+    private HttpServletResponse createResponse() throws Exception {
+        HttpServletResponse mockResponse = Mockito.mock(HttpServletResponse.class);
+
+        doNothing().when(mockResponse).sendRedirect(isA(String.class));
+        doNothing().when(mockResponse).sendError(isA(Integer.class), isA(String.class));
+
+        return mockResponse;
+    }
+}