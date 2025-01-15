DSpace/DSpace

Minor checkstyle fixes after bump to 8.45.1. All are indentation / spacing fixes which are more strict now.

dspace-server-webapp/src/main/java/org/dspace/app/rest/model/ItemRest.java
@@ -17,46 +17,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author Andrea Bollini (andrea.bollini at 4science.it)
  */
 @LinksRest(links = {
-        @LinkRest(
-                name = ItemRest.ACCESS_STATUS,
-                method = "getAccessStatus"
-        ),
-        @LinkRest(
-                name = ItemRest.BUNDLES,
-                method = "getBundles"
-        ),
-        @LinkRest(
-                name = ItemRest.IDENTIFIERS,
-                method = "getIdentifiers"
-        ),
-        @LinkRest(
-                name = ItemRest.MAPPED_COLLECTIONS,
-                method = "getMappedCollections"
-        ),
-        @LinkRest(
-                name = ItemRest.OWNING_COLLECTION,
-                method = "getOwningCollection"
-        ),
-        @LinkRest(
-                name = ItemRest.RELATIONSHIPS,
-                method = "getRelationships"
-        ),
-        @LinkRest(
-                name = ItemRest.VERSION,
-                method = "getItemVersion"
-        ),
-        @LinkRest(
-                name = ItemRest.TEMPLATE_ITEM_OF,
-                method = "getTemplateItemOf"
-        ),
-        @LinkRest(
-                name = ItemRest.THUMBNAIL,
-                method = "getThumbnail"
-        ),
-        @LinkRest(
-        name = ItemRest.SUBMITTER,
-        method = "getItemSubmitter"
-        )
+    @LinkRest(name = ItemRest.ACCESS_STATUS, method = "getAccessStatus"),
+    @LinkRest(name = ItemRest.BUNDLES, method = "getBundles"),
+    @LinkRest(name = ItemRest.IDENTIFIERS, method = "getIdentifiers"),
+    @LinkRest(name = ItemRest.MAPPED_COLLECTIONS, method = "getMappedCollections"),
+    @LinkRest(name = ItemRest.OWNING_COLLECTION, method = "getOwningCollection"),
+    @LinkRest(name = ItemRest.RELATIONSHIPS, method = "getRelationships"),
+    @LinkRest(name = ItemRest.VERSION, method = "getItemVersion"),
+    @LinkRest(name = ItemRest.TEMPLATE_ITEM_OF, method = "getTemplateItemOf"),
+    @LinkRest(name = ItemRest.THUMBNAIL, method = "getThumbnail"),
+    @LinkRest(name = ItemRest.SUBMITTER, method = "getItemSubmitter")
 })
 public class ItemRest extends DSpaceObjectRest {
     public static final String NAME = "item";

pinokew/DSpace-docker

dspace-server-webapp/src/main/java/org/dspace/app/rest/model/ItemRest.java
@@ -17,42 +17,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author Andrea Bollini (andrea.bollini at 4science.it)
  */
 @LinksRest(links = {
-        @LinkRest(
-                name = ItemRest.ACCESS_STATUS,
-                method = "getAccessStatus"
-        ),
-        @LinkRest(
-                name = ItemRest.BUNDLES,
-                method = "getBundles"
-        ),
-        @LinkRest(
-                name = ItemRest.IDENTIFIERS,
-                method = "getIdentifiers"
-        ),
-        @LinkRest(
-                name = ItemRest.MAPPED_COLLECTIONS,
-                method = "getMappedCollections"
-        ),
-        @LinkRest(
-                name = ItemRest.OWNING_COLLECTION,
-                method = "getOwningCollection"
-        ),
-        @LinkRest(
-                name = ItemRest.RELATIONSHIPS,
-                method = "getRelationships"
-        ),
-        @LinkRest(
-                name = ItemRest.VERSION,
-                method = "getItemVersion"
-        ),
-        @LinkRest(
-                name = ItemRest.TEMPLATE_ITEM_OF,
-                method = "getTemplateItemOf"
-        ),
-        @LinkRest(
-                name = ItemRest.THUMBNAIL,
-                method = "getThumbnail"
-        )
+    @LinkRest(name = ItemRest.ACCESS_STATUS, method = "getAccessStatus"),
+    @LinkRest(name = ItemRest.BUNDLES, method = "getBundles"),
+    @LinkRest(name = ItemRest.IDENTIFIERS, method = "getIdentifiers"),
+    @LinkRest(name = ItemRest.MAPPED_COLLECTIONS, method = "getMappedCollections"),
+    @LinkRest(name = ItemRest.OWNING_COLLECTION, method = "getOwningCollection"),
+    @LinkRest(name = ItemRest.RELATIONSHIPS, method = "getRelationships"),
+    @LinkRest(name = ItemRest.VERSION, method = "getItemVersion"),
+    @LinkRest(name = ItemRest.TEMPLATE_ITEM_OF, method = "getTemplateItemOf"),
+    @LinkRest(name = ItemRest.THUMBNAIL, method = "getThumbnail")
 })
 public class ItemRest extends DSpaceObjectRest {
     public static final String NAME = "item";